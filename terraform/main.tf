020f6bd34613259bce262f6f5144721bc9043d83
mmdcloud <madmaxcloudpractice@gmail.com>
2025-11-08 16:02:22 +0530

    update: refactor VPC and security group configurations; add S3 logging modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,28 +34,28 @@ module "ec2_log_group" {
 # VPC configuration
 # -----------------------------------------------------------------------------------------
 module "vpc" {
-  source = "./modules/vpc"
-  vpc_name = "vpc"
-  vpc_cidr = "10.0.0.0/16"
-  azs             = var.azs
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  create_igw = true
+  source                  = "./modules/vpc"
+  vpc_name                = "vpc"
+  vpc_cidr                = "10.0.0.0/16"
+  azs                     = var.azs
+  public_subnets          = var.public_subnets
+  private_subnets         = var.private_subnets
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  create_igw              = true
   map_public_ip_on_launch = true
-  enable_nat_gateway     = false
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = false
+  enable_nat_gateway      = false
+  single_nat_gateway      = false
+  one_nat_gateway_per_az  = false
   tags = {
-    Project     = "nodeapp"
+    Project = "nodeapp"
   }
 }
 
 # Security Group
 resource "aws_security_group" "ecs_lb_sg" {
-  name        = "ecs-lb-sg"
-  vpc_id      = module.vpc.vpc_id
+  name   = "ecs-lb-sg"
+  vpc_id = module.vpc.vpc_id
 
   ingress {
     description = "HTTP traffic"
@@ -78,14 +78,14 @@ resource "aws_security_group" "ecs_lb_sg" {
 }
 
 resource "aws_security_group" "ecs_sg" {
-  name        = "ecs-sg"
-  vpc_id      = module.vpc.vpc_id
+  name   = "ecs-sg"
+  vpc_id = module.vpc.vpc_id
 
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [aws_security_group.ecs_lb_sg.id]
   }
 
   egress {
@@ -101,8 +101,8 @@ resource "aws_security_group" "ecs_sg" {
 }
 
 resource "aws_security_group" "asg_lb_sg" {
-  name        = "asg-lb-sg"
-  vpc_id      = module.vpc.vpc_id
+  name   = "asg-lb-sg"
+  vpc_id = module.vpc.vpc_id
 
   ingress {
     from_port   = 80
@@ -124,14 +124,14 @@ resource "aws_security_group" "asg_lb_sg" {
 }
 
 resource "aws_security_group" "asg_sg" {
-  name        = "asg-sg"
-  vpc_id      = module.vpc.vpc_id
+  name   = "asg-sg"
+  vpc_id = module.vpc.vpc_id
 
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [aws_security_group.asg_lb_sg.id]
   }
 
   egress {
@@ -223,6 +223,55 @@ module "lambda_function" {
 }
 
 # -----------------------------------------------------------------------------------------
+# S3 configuration
+# -----------------------------------------------------------------------------------------
+module "ecs_lb_logs" {
+  source        = "./modules/s3"
+  bucket_name   = "ecs-lb-logs"
+  objects       = []
+  bucket_policy = ""
+  cors = [
+    {
+      allowed_headers = ["*"]
+      allowed_methods = ["GET"]
+      allowed_origins = ["*"]
+      max_age_seconds = 3000
+    },
+    {
+      allowed_headers = ["*"]
+      allowed_methods = ["PUT"]
+      allowed_origins = ["*"]
+      max_age_seconds = 3000
+    }
+  ]
+  versioning_enabled = "Enabled"
+  force_destroy      = true
+}
+
+module "asg_lb_logs" {
+  source        = "./modules/s3"
+  bucket_name   = "asg-lb-logs"
+  objects       = []
+  bucket_policy = ""
+  cors = [
+    {
+      allowed_headers = ["*"]
+      allowed_methods = ["GET"]
+      allowed_origins = ["*"]
+      max_age_seconds = 3000
+    },
+    {
+      allowed_headers = ["*"]
+      allowed_methods = ["PUT"]
+      allowed_origins = ["*"]
+      max_age_seconds = 3000
+    }
+  ]
+  versioning_enabled = "Enabled"
+  force_destroy      = true
+}
+
+# -----------------------------------------------------------------------------------------
 # ECS & ECR configuration
 # -----------------------------------------------------------------------------------------
 module "ecr_container_registry" {
@@ -235,46 +284,50 @@ module "ecr_container_registry" {
 }
 
 module "ecs_lb" {
-  source                     = "./modules/load-balancer"
-  lb_name                    = "ecs-lb"
-  lb_is_internal             = false
-  lb_ip_address_type         = "ipv4"
+  source                     = "terraform-aws-modules/alb/aws"
+  name                       = "ecs-lb"
   load_balancer_type         = "application"
-  enable_deletion_protection = false
-  security_groups            = [aws_security_group.ecs_lb_sg.id]
+  vpc_id                     = module.vpc.vpc_id
   subnets                    = module.vpc.public_subnets
-  target_groups = [
-    {
-      target_group_name                = "ecs-lb-tg"
-      target_port                      = 3000
-      target_ip_address_type           = "ipv4"
-      target_protocol                  = "HTTP"
-      target_type                      = "ip"
-      target_vpc_id                    = module.vpc.vpc_id
-      health_check_interval            = 30
-      health_check_path                = "/"
-      health_check_enabled             = true
-      health_check_protocol            = "HTTP"
-      health_check_timeout             = 5
-      health_check_healthy_threshold   = 3
-      health_check_unhealthy_threshold = 3
-      health_check_port                = 3000
-
-    }
+  enable_deletion_protection = false
+  drop_invalid_header_fields = true
+  ip_address_type            = "ipv4"
+  internal                   = false
+  security_groups = [
+    aws_security_group.ecs_lb_sg.id
   ]
-  listeners = [
-    {
-      listener_port     = 80
-      listener_protocol = "HTTP"
-      certificate_arn   = null
-      default_actions = [
-        {
-          type             = "forward"
-          target_group_arn = module.ecs_lb.target_groups[0].arn
-        }
-      ]
+  access_logs = {
+    bucket = "${module.ecs_lb_logs.bucket}"
+  }
+  listeners = {
+    ecs_lb_http_listener = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_key = "ecs_lb_tg"
+      }
     }
-  ]
+  }
+  target_groups = {
+    ecs_lb_tg = {
+      backend_protocol = "HTTP"
+      backend_port     = 3000
+      target_type      = "ip"
+      health_check = {
+        enabled             = true
+        healthy_threshold   = 3
+        interval            = 30
+        path                = "/"
+        port                = 3000
+        protocol            = "HTTP"
+        unhealthy_threshold = 3
+      }
+      create_attachment = false
+    }
+  }
+  tags = {
+    Project = "centralized-logging"
+  }
 }
 
 resource "aws_ecs_cluster" "ecs_cluster" {
@@ -421,6 +474,132 @@ module "ecs" {
   assign_public_ip = true
 }
 
+module "ecs" {
+  source       = "terraform-aws-modules/ecs/aws"
+  cluster_name = "text-to-sql-cluster"
+  default_capacity_provider_strategy = {
+    FARGATE = {
+      weight = 50
+      base   = 20
+    }
+    FARGATE_SPOT = {
+      weight = 50
+    }
+  }
+  autoscaling_capacity_providers = {
+    ASG = {
+      auto_scaling_group_arn         = module.autoscaling.autoscaling_group_arn
+      managed_draining               = "ENABLED"
+      managed_termination_protection = "ENABLED"
+
+      managed_scaling = {
+        maximum_scaling_step_size = 5
+        minimum_scaling_step_size = 1
+        status                    = "ENABLED"
+        target_capacity           = 60
+      }
+    }
+  }
+
+  services = {
+    ecs-frontend = {
+      cpu    = 1024
+      memory = 4096
+      # Container definition(s)
+      container_definitions = {
+        fluent-bit = {
+          cpu       = 512
+          memory    = 1024
+          essential = true
+          image     = nonsensitive(data.aws_ssm_parameter.fluentbit.value)
+          user      = "0"
+          firelensConfiguration = {
+            type = "fluentbit"
+          }
+          memoryReservation                      = 50
+          cloudwatch_log_group_retention_in_days = 30
+        }
+
+        (ecs) = {
+          cpu       = 1024
+          memory    = 2048
+          essential = true
+          image     = "${module.frontend_container_registry.repository_url}:latest"
+          placementStrategy = [
+            {
+              type  = "spread",
+              field = "attribute:ecs.availability-zone"
+            }
+          ]
+          healthCheck = {
+            command = ["CMD-SHELL", "curl -f http://localhost:3000/auth/signin || exit 1"]
+          }
+          ulimits = [
+            {
+              name      = "nofile"
+              softLimit = 65536
+              hardLimit = 65536
+            }
+          ]
+          portMappings = [
+            {
+              name          = "ecs-frontend"
+              containerPort = 3000
+              hostPort      = 3000
+              protocol      = "tcp"
+            }
+          ]
+          environment = [
+            {
+              name  = "BASE_URL"
+              value = "${module.backend_lb.dns_name}"
+            }
+          ]
+          capacity_provider_strategy = {
+            ASG = {
+              base              = 20
+              capacity_provider = "ASG"
+              weight            = 50
+            }
+          }
+          # Example image used requires access to write to root filesystem
+          readonlyRootFilesystem = false
+          dependsOn = [{
+            containerName = "fluent-bit"
+            condition     = "START"
+          }]
+          enable_cloudwatch_logging = false
+          logConfiguration = {
+            logDriver = "awsfirelens"
+            options = {
+              Name                    = "firehose"
+              region                  = var.region
+              delivery_stream         = "ecs-stream"
+              log-driver-buffer-limit = "2097152"
+            }
+          }
+          memoryReservation = 100
+          restartPolicy = {
+            enabled              = true
+            ignoredExitCodes     = [1]
+            restartAttemptPeriod = 60
+          }
+        }
+      }
+      load_balancer = {
+        service = {
+          target_group_arn = module.ecs_lb.target_groups["ecs_lb_target_group"].arn
+          container_name   = "ecs-frontend"
+          container_port   = 3000
+        }
+      }
+      subnet_ids                    = module.vpc.private_subnets
+      vpc_id                        = module.vpc.vpc_id
+      availability_zone_rebalancing = "ENABLED"
+    }
+  }
+}
+
 # -----------------------------------------------------------------------------------------
 # EC2 & Autoscaling groups configuration
 # -----------------------------------------------------------------------------------------
@@ -496,45 +675,50 @@ module "asg" {
 }
 
 module "asg_lb" {
-  source                     = "./modules/load-balancer"
-  lb_name                    = "asg-lb"
-  lb_is_internal             = false
-  lb_ip_address_type         = "ipv4"
+  source                     = "terraform-aws-modules/alb/aws"
+  name                       = "asg-lb"
   load_balancer_type         = "application"
-  enable_deletion_protection = false
-  security_groups            = [aws_security_group.asg_lb_sg.id]
+  vpc_id                     = module.vpc.vpc_id
   subnets                    = module.vpc.public_subnets
-  target_groups = [
-    {
-      target_group_name                = "nodeapp"
-      target_port                      = 80
-      target_ip_address_type           = "ipv4"
-      target_protocol                  = "HTTP"
-      target_type                      = "instance"
-      target_vpc_id                    = module.vpc.vpc_id
-      health_check_interval            = 30
-      health_check_path                = "/"
-      health_check_enabled             = true
-      health_check_protocol            = "HTTP"
-      health_check_timeout             = 5
-      health_check_healthy_threshold   = 3
-      health_check_unhealthy_threshold = 3
-      health_check_port                = 80
-    }
+  enable_deletion_protection = false
+  drop_invalid_header_fields = true
+  ip_address_type            = "ipv4"
+  internal                   = false
+  security_groups = [
+    aws_security_group.asg_lb_sg.id
   ]
-  listeners = [
-    {
-      listener_port     = 80
-      listener_protocol = "HTTP"
-      certificate_arn   = null
-      default_actions = [
-        {
-          type             = "forward"
-          target_group_arn = module.asg_lb.target_groups[0].arn
-        }
-      ]
+  access_logs = {
+    bucket = "${module.asg_lb_logs.bucket}"
+  }
+  listeners = {
+    asg_lb_http_listener = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_key = "asg_lb_tg"
+      }
     }
-  ]
+  }
+  target_groups = {
+    asg_lb_tg = {
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      health_check = {
+        enabled             = true
+        healthy_threshold   = 3
+        interval            = 30
+        path                = "/"
+        port                = 80
+        protocol            = "HTTP"
+        unhealthy_threshold = 3
+      }
+      create_attachment = false
+    }
+  }
+  tags = {
+    Project = "centralized-logging"
+  }
 }
 
 # -----------------------------------------------------------------------------------------
